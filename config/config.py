2384cb60d796aa91c490bb0607b494bdb340856d
twlght <liuhaotian.twilight@gmail.com>
2018-02-10 20:57:43 +0800

    更改postgres在linux上的密码

--- config/config.py
+++ config/config.py
@@ -6,4 +6,4 @@ topics_per_page = 40
 if platform.system() == 'Windows':
     database_uri = 'postgresql://postgres:root@127.0.0.1:5432/bbsdb'
 elif platform.system() == 'Linux':
-    database_uri = 'postgresql://postgres:postgres@127.0.0.1:5432/bbsdb'
+    database_uri = 'postgresql://postgres:root@127.0.0.1:5432/bbsdb'
