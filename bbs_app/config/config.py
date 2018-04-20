3713ba9178ae236cad5e0155d181dc2d916285e5
twlght <liuhaotian.twilight@gmail.com>
2018-04-20 16:41:37 +0800

    加入docker 更改了文件结构

--- bbs_app/config/config.py
+++ bbs_app/config/config.py
@@ -3,4 +3,4 @@ secret_key = 'bbs'
 if platform.system() == 'Windows':
     database_uri = 'postgresql://postgres:root@127.0.0.1:5432/bbsdb'
 elif platform.system() == 'Linux':
-    database_uri = 'postgresql://postgres:root@bbsdatabase:5432/bbsdb'
+    database_uri = 'postgresql://postgres:root@127.0.0.1:5432/postgres'
