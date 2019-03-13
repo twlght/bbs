bd2c1788d5ca8a9254271f80c61a04a437833eb7
twlght <liuhaktian.twilight@gmail.com>
2019-03-13 18:16:03 +0800

    修改了 docker-compose的挂载方式 将代码也做挂载处理

--- bbs_app/config/config.py
+++ bbs_app/config/config.py
@@ -4,12 +4,12 @@ if platform.system() == 'Windows':
     database_uri = 'postgresql://postgres:root@127.0.0.1:5432/bbsdb'
 elif platform.system() == 'Linux':
     # database_uri = 'postgresql://postgres:root@bbsdb:5432/postgres'
-    database_uri = 'postgresql://postgres:postgres@bbsdb:5432/postgres'
+    # database_uri = 'postgresql://postgres:postgres@bbsdb:5432/postgres'
 
     # bbsdb为db容器别名,
 
     # 使用MySQL数据库
-    # database_uri = 'mysql://'
+    database_uri = 'mysql://root:mysql123@bbsdb:3306/mysqldb'
 
 # MySQL mysql://username:password@hostname/database
 # Postgres postgresql://username:password@hostname/database
